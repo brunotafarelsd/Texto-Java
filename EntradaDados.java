009c805c2c3260c5233c89a4f5d28bd1872ffcf3
Your Name <you@example.com>
2020-08-04 17:36:49 -0300

    Adicionado cálculo de subtração

--- EntradaDados.java
+++ EntradaDados.java
@@ -13,12 +13,14 @@ public class EntradaDados{
         double n2 = sc.nextDouble();
         // Aqui o usuário insere o número e o scanner identifica e salva na variável "n2"
 
-        double soma = n1 + n2;
-        // aqui foi criada a variável soma, que terá a função de guardar o resultado da soma do n1 + n2
+        double subtracao = n1 - n2;
+        // aqui foi criada a variável subtracao, que terá a função de guardar o resultado da operação
 
-        System.out.println("O resultado da soma: " + soma); 
+        System.out.println("O resultado da subtracao: " + subtracao); 
 
         //informado o resultado final para o usuário
+
+
         
 
         sc.close();
